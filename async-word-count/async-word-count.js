13ee47e1d49838e88a25e5e4c57294ad8f2b800f
khchoi <khchoi0301@gmail.com>
2018-10-20 09:51:23 +0900

    complete async

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 
-var getWordCount = function(filePath, callback) {
-  fs.readFile(filePath, 'utf-8', function(err, data) {
+var getWordCount = function (filePath, callback) {
+  fs.readFile(filePath, 'utf-8', function (err, data) {
     if (err) {
       callback(err, null);
       return;
@@ -13,8 +13,19 @@ var getWordCount = function(filePath, callback) {
   });
 };
 
-var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+var getTotalWordCount = function (filePathOne, filePathTwo, callback) {
+  var total = 0
+  getWordCount(filePathOne, (err, data1) => {
+    if (err) { throw err }
+    // console.log(err, data1)
+    total += data1
+    getWordCount(filePathTwo, (err, data2) => {
+      if (err) { throw err }
+      // console.log(err, data2)
+      total += data2
+      callback(total)
+    })
+  })
 };
 
 module.exports = getTotalWordCount;
