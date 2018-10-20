48ed96b2a3bbc1ec854b26dcf70429d13000d9fb
khchoi <khchoi0301@gmail.com>
2018-10-20 10:00:32 +0900

    complete node

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -10,9 +10,21 @@ var User = sequelize.define('User', {
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
 
+app.get('/user', (req, res) => {
 
+  // console.log(User.findAll())
+  User.findAll()
+    .then((data) => {
+      const users = []
+      for (let i = 0; i < data.length; i++) {
+        users.push(data[i].username)
+        console.log(data[i].username)
+      }
+      res.send(users)
+    })
+})
 
-module.exports = { 
+module.exports = {
   app: app,
   User: User
 };
